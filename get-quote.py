11ed63acec9a0d6a1361143d54bd422fa684b34e
Shad6owh4rd <85156719+Shad6owh4rd@users.noreply.github.com>
2021-06-12 09:34:02 +0200

    "More than one quota at a time"

--- get-quote.py
+++ get-quote.py
@@ -10,6 +10,8 @@ def primary():
   last = len(quotes) - 1
   rnd = random.randint(0, last)
   print(quotes[rnd])
+  rnd = random.randint(0, last)
+  print(quotes[rnd])
 
 if __name__== "__main__":
   primary()
